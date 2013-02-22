14ea7d1b7a9761f51ff3fc9704828ed2f11fed97
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-02-22 21:06:58 +0100

    O1 optimization by default

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 OUTPUT=http-parser.js
 EXPORTED_FUNCTIONS="['_create_parser', '_get_settings', '_http_parser_execute', '_get_error_name', '_get_error_description', '_http_should_keep_alive', '_get_http_major', '_get_http_minor', '_get_status_code', '_get_method', '_get_upgrade']"
 PARSER_DIR="dep/http-parser"
-OPTIMIZATION="g"
+OPTIMIZATION="O1"
 
 ../emscripten/emcc $PARSER_DIR/http_parser.c ./wrapper.c -I$PARSER_DIR -s EXPORTED_FUNCTIONS="$EXPORTED_FUNCTIONS" -$OPTIMIZATION -o parser.js
 
